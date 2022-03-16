dde6c53be0a573f9cf3591f37bbb9e321e8981f3
Evgeniy Surov <surovjeniya@gmail.com>
2022-03-16 15:13:45 +0300

    feat:added basic pages

--- fortnite-shop/src/components/App/App.jsx
+++ fortnite-shop/src/components/App/App.jsx
@@ -1,7 +1,13 @@
 import React,{useState,useEffect} from 'react'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import { AppContext } from '../../context/AppContext';
 import {getShop} from '../../services/api'
 import './App.scss'
+//components
+import Header from '../Header'
+import Home from '../../pages/Home'
+import Cart from '../../pages/Cart'
+import NotFound from '../../pages/NotFound'
 
 export default function App () {
 
@@ -19,7 +25,16 @@ export default function App () {
     return (
         <AppContext.Provider value = {value}>
             <div className="app">
-            
+                <Router>
+                    <Header/>
+                    <main className="main">
+                        <Routes>
+                            <Route exact path='/' element = {<Home/>}/>
+                            <Route  path='/cart' element = {<Cart/>}/>
+                            <Route  path='*' element = {<NotFound/>}/>
+                        </Routes>
+                    </main>
+                </Router>
             </div>
         </AppContext.Provider>
     )
---
+++ fortnite-shop/src/pages/Cart/Cart.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Cart.scss';
+
+export default function Cart () {
+    return (
+        <div className="cart">
+            ...cart
+        </div>
+    )
+}
