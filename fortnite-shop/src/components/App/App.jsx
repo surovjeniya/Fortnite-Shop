09318eafb6cb4e185513a99b0e80d6160ec13aa3
Evgeniy Surov <surovjeniya@gmail.com>
2022-03-16 14:56:10 +0300

    feat:added API

--- fortnite-shop/src/components/App/App.jsx
+++ fortnite-shop/src/components/App/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { AppContext } from '../../context/AppContext';
+import {getShop} from '../../services/api'
 import './App.scss'
 
 export default function App () {
 
+    const [shopItems,setShopItems] = useState([])
+    
+    useEffect(() => {
+        getShop().then(data => setShopItems(data.shop))
+        
+    },[])
 
     const value = {
         example:''
